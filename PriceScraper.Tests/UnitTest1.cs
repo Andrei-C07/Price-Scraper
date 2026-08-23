49d3afd1fd3bc82cf0fcb797238e47b849fb9f08
Andrei Cretu <andrei.cretu4676@gmail.com>
2026-08-23 13:26:26 +0300

    initial commit

---
+++ PriceScraper.Tests/UnitTest1.cs
@@ -0,0 +1,52 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using NUnit.Framework;
+using OpenQA.Selenium.Support.UI;
+
+namespace PriceScraper.Tests;
+
+public class Tests
+{
+    private IWebDriver driver;
+    private WebDriverWait wait;
+
+    [SetUp]
+    public void Setup()
+    {
+        driver = new FirefoxDriver();
+        wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        driver.Navigate().GoToUrl("https://www.saucedemo.com");
+    }
+
+    [Test]
+    public void SauseDemo_LoadsSuccesfully()
+    {
+        driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+        Assert.That(driver.Title, Is.EqualTo("Swag Labs"));
+        
+        var loginButton = driver.FindElement(By.Id("login-button"));
+        Assert.That(loginButton.Displayed, Is.True);
+    }
+
+    [Test]
+    public void ValidLogin_RedirectsToInventoryPage()
+    {
+        Login("standard_user", "secret_sauce");
+        Assert.That(driver.Url, Is.EqualTo("https://www.saucedemo.com/inventory.html"));
+    }
+
+
+    [TearDown]
+    public void TearDown()
+    {
+        driver?.Quit();
+    }
+
+    private void Login(string username, string password)
+    {
+        driver.FindElement(By.Id("user-name")).SendKeys(username);
+        driver.FindElement(By.Id("password")).SendKeys(password);
+        
+        driver.FindElement(By.Id("login-button")).Click();
+    }
+}
